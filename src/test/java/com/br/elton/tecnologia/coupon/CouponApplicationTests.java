56368c00b6c2c46bf2bb7fd1abfe92e8a75d9480
Elton Henrique de Oliveira <terezafe.oli@gmail.com>
2025-12-23 21:48:41 -0300

    Initial commit

---
+++ src/test/java/com/br/elton/tecnologia/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.elton.tecnologia.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
